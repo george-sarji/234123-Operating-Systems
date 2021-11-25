13b9f6f188418b23b8aeed756e7acc0cf45df4cd
george-sarji <georgesarji@gmail.com>
2021-11-26 01:10:36 +0200

    Fixed issues with kill command

--- hw1/BuiltIn.cpp
+++ hw1/BuiltIn.cpp
@@ -47,7 +47,7 @@ void ChangeDirCommand::execute()
             cout << "smash error: cd: OLDPWD not set " << endl;
             return;
         }
-        future_path= smash.paths.back();
+        future_path = smash.paths.back();
         smash.paths.pop_back();
     }
     else
@@ -56,12 +56,12 @@ void ChangeDirCommand::execute()
     }
     // Now we have to change the directory.
     int result = chdir(future_path.c_str());
-    if(result != 0) 
+    if (result != 0)
     {
         // Send out an error.
         cout << strerror(errno) << endl;
     }
-    else 
+    else
     {
         // We have to push the old path into the path vector.
         smash.paths.push_back(current_path);
@@ -72,36 +72,48 @@ void KillCommand::execute()
 {
     SmallShell &smash = SmallShell::getInstance();
     vector<string> args = smash.curr_arguments;
-
-    if (!args[3].empty() || args[1] != "-9" || args[2].empty())
+    // We have to check the arguments.
+    // Check if we have a second and a third argument.
+    if (args[1].empty() || args[2].empty() || !args[3].empty())
     {
+        cout << "smash error: kill: invalid arguments" << endl;
+    }
+    // We have to check the format of the first argument (the actual signal number)
+    string signal_str = args[1];
+    string signal_num = signal_str.substr(1, signal_str.length());
+    string job_str = args[2];
+    // If we have an invalid argument format, exit.
+    if (signal_str[0] != '-' || !isNumber(signal_num) || !isNumber(job_str))
+    {
+        // Invalid format for argument. Exit.
         cout << "smash error: kill: invalid arguments" << endl;
         return;
     }
-    string job_id = args[2];
-    if (isNumber(args[2]))
+    // Check for the job ID in the jobs list.
+    int signal = stoi(signal_num);
+    JobsList::JobEntry *job = smash.jobs->getJobById(stoi(job_str));
+    if (job == nullptr)
     {
-        if (smash.jobs->getJobById(stoi(job_id)))
-        {
-            cout << "i am here  in kill " << endl;
-            cout << "the proc id is " << jobsList->getJobById(stoi(job_id))->p_id << endl;
-            kill(jobsList->getJobById(stoi(job_id))->p_id, 9);
-            jobsList->removeJobById(stoi(job_id));
-            return;
-        }
-        else
-        {
-            if (isNumber(args[2]))
-            {
-                cout << "smash error: kill: job-id " << args[2] << " does not exist" << endl;
-            }
-            else
-                cout << "smash error: kill: invalid arguments" << endl;
-        }
+        // Invalid job id. Exit.
+        cout << "smash error: kill: job-id " << job_str << " does not exist" << endl;
+        return;
     }
-    else
+    // We can now send the signal to the process.
+    int result = kill(job->p_id, stoi(signal_num));
+    if (result != 0)
     {
-        cout << "smash error: kill: invalid arguments" << endl;
+        // We have an error.
+        cout << strerror(errno) << endl;
+        return;
+    }
+    // Worked. Send a message.
+    cout << "signal number " << signal_num << " was sent to pid " << job->p_id << endl;
+    // Update the job if it's a stopping signal.
+    if (signal == SIGTSTP || signal == SIGINT || signal == SIGSTOP)
+    {
+        cout << "Stopped with " << signal_num << endl;
+        // Change the job to stopped.
+        job->stop();
     }
 }
 
