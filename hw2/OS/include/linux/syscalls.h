66910f5a4c70576242d2fe32ef8293c842c63cf2
george-sarji <georgesarji@gmail.com>
2021-12-13 18:44:34 +0000

    Added syscall declaration

--- hw2/OS/include/linux/syscalls.h
+++ hw2/OS/include/linux/syscalls.h
@@ -941,5 +941,7 @@ asmlinkage long sys_pkey_free(int pkey);
 asmlinkage long sys_statx(int dfd, const char __user *path, unsigned flags,
 			  unsigned mask, struct statx __user *buffer);
 asmlinkage long sys_hello(void);
+asmlinkage int sys_get_weight(void);
+asmlinkage pid_t sys_get_heaviest_ancestor(void);
 
 #endif
