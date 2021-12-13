d3a3a603b7ae660a2873ff6e282057f0c90fd880
ahmad.adawi <ahmad.adawi@campus.technion.ac.il>
2021-12-13 20:55:21 +0200

    Updated files to latest

--- hw2/OS/kernel/hw2.c
+++ hw2/OS/kernel/hw2.c
@@ -5,3 +5,16 @@ asmlinkage long sys_hello(void)
     printk("Hello, World!\n");
     return 0;
 }
+
+asmlinkage int sys_set_weight(int weight){
+
+    if (weight < 0 ){
+        return -EINVAL;
+    }
+    current->weight = weight;
+    return 0;
+}
+
+asmlinkage int sys_get_leaf_children_sum(void){
+
+}
